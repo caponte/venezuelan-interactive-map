bbed2e3dd5b84265254b7b43679a2ed90007699a
Carlos Aponte <carlos.apo1692@gmail.com>
2024-07-30 14:13:50 -0400

    Interactive Map Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { InteractiveMapComponent } from "./interactive-map/interactive-map.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [InteractiveMapComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'interactive-venezuelan-map';
+  title = 'Interactive Venezuelan Map';
 }
