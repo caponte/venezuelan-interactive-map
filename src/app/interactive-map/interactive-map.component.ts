c85a1920fd7e65840760d0c84af519a3ac134621
Carlos Aponte <carlos.apo1692@gmail.com>
2024-07-31 16:40:19 -0400

    Missing setups

--- src/app/interactive-map/interactive-map.component.ts
+++ src/app/interactive-map/interactive-map.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-interactive-map',
-  standalone: true,
-  imports: [],
   templateUrl: './interactive-map.component.html',
   styleUrl: './interactive-map.component.css'
 })
 export class InteractiveMapComponent {
 
+  OnSelectState( state:string ): void{
+    console.log(state);
+  }
 }
